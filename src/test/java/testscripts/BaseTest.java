1b0e3259b3a330f898eb12f561ddbed3d7a21baf
aravindanath <20963786+aravindanath@users.noreply.github.com>
2022-11-27 09:42:50 +0530

    final code

--- src/test/java/testscripts/BaseTest.java
+++ src/test/java/testscripts/BaseTest.java
@@ -1,26 +1,52 @@
 package testscripts;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 import pages.BasePage;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest
 {
+
     protected WebDriver driver;
+    protected static ExtentReports reports;
+    protected ExtentSparkReporter spark;
+
+    @BeforeSuite
+    public void report(){
+        reports = new ExtentReports();
+        spark = new ExtentSparkReporter("./index.html");
+        spark.config().setDocumentTitle("Automation Reports");
+        spark.config().setReportName("Arvind");
+        spark.config().setTheme(Theme.STANDARD);
+        reports.attachReporter(spark);
+
+    }
+    @AfterSuite
+    public void endReport(){
+        reports.flush();
+    }
     @BeforeClass
     public void setup(){
-         WebDriverManager.chromedriver().setup();
-         driver = new ChromeDriver();
-         driver.manage().window().maximize();
-         driver.get(BasePage.getValues("url"));
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get(BasePage.getValues("url"));
     }
 
     @AfterClass
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(5000);
+    public void teardown() throws InterruptedException {
+        Thread.sleep(6000);
         driver.quit();
     }
 }
--- src/test/java/testscripts/TC_RF_001.java
+++ src/test/java/testscripts/TC_RF_001.java
@@ -1,5 +1,6 @@
 package testscripts;
 
+import com.aventstack.extentreports.ExtentTest;
 import org.testng.annotations.Test;
 import pages.BasePage;
 import pages.HomePage;
@@ -17,12 +18,16 @@ public class TC_RF_001 extends BaseTest{
 
         String fn = BasePage.firstname(),ln = BasePage.lastName(), mobile = BasePage.phonenumeber(),email = BasePage.emailid(),password = BasePage.password();
 
-
+        ExtentTest test = reports.createTest("TC_RF_001");
         HomePage hm = new HomePage(driver);
+        test.info(driver.getTitle());
+        test.info("user is navigating to register page");
         hm.navigateToRegister();
-
+        test.info("user is navigated to register page");
         RegisterPage rp = new RegisterPage(driver);
+        test.info("user is filling data to register new user");
         rp.registerNewUser(fn,ln,email,mobile,password,password);
+        test.info("user is sucessfully  registered new user");
 
     }
 }
